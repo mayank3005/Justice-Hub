31694524c1c658676cf410e0f43551c091479dea
Silver094 <deepanshuvishwakarma14@gmail.com>
2023-02-25 21:39:45 +0530

    added payment gateway

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createContext, useReducer } from "react";
 import { Route, Routes } from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
@@ -13,24 +13,33 @@ import Login from "./pages/Login";
 import Test from "./pages/Test";
 
 import Register from "./pages/Register";
+import { initialState, reducer } from "./reducer/UserReducer";
+import Logout from "./pages/Logout";
+
+export const userContext = createContext();
 
 const App = () => {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (<>
-    <div className="allButFooter">
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/newsletter' element={<Newsletter />} />
-        <Route path='/getinvolved' element={<Getinvolved />} />
-        <Route path='/donate' element={<Donate />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/test' element={<Test />} />
-        <Route path='*' element={<Home />} />
-      </Routes>
-    </div>
-    <Footer />
+    <userContext.Provider value={{ state, dispatch }}>
+      <div className="allButFooter">
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/newsletter' element={<Newsletter />} />
+          <Route path='/getinvolved' element={<Getinvolved />} />
+          <Route path='/donate' element={<Donate />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/logout' element={<Logout />} />
+          <Route path='*' element={<Home />} />
+        </Routes>
+      </div>
+      <Footer />
+    </userContext.Provider>
   </>);
 };
 
